7eb6c5a3882fb7847fa0c2c5b7337842fba11878
fyrkantis <david@kniberg.com>
2022-01-18 21:50:54 +0100

    Reworked slash command error handling.

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 # External Libraries
+from calendar import c
 import os
 from dotenv import load_dotenv
 
@@ -10,7 +11,9 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client()
 
-tournament = None
+tournaments = {}
+
+# Main classes.
 
 class Tournament():
 	def __init__(self, guildId, textId, voiceId):
@@ -45,6 +48,8 @@ class Player():
 	def __init__(self, id):
 		id = id
 
+# Basic events.
+
 @client.event
 async def on_ready():
 	print(f"{client.user} has connected to Discord!")
@@ -70,10 +75,12 @@ async def on_message(message):
 		if "tournament bot" in message.content.lower():
 			await message.reply("Hello World!")
 
-# Tries to find the guild a slash command originated from, and returns feedback if unable to.
+# Lots of fancy help functions for slash commands.
+
+# Just tries to find the guild a slash command originated from.
 def findGuild(guildId, action = "do this"):
 	if guildId is None:
-		return f"Sorry, can't {action} in DMs."
+		return None, f"Sorry, you can't {action} in DMs."
 	guild = None
 	global client # TODO: Use context.me instead.
 	for joinedGuild in client.guilds:
@@ -81,8 +88,24 @@ def findGuild(guildId, action = "do this"):
 			guild = joinedGuild
 			break
 	if guild is None:
-		return f"You need to invite Tournament Bot to {action}.\nInvite link: https://discord.com/api/oauth2/authorize?client_id=931495315834548244&permissions=285379664&scope=bot%20applications.commands"
-	return guild
+		return None, f"You need to invite Tournament Bot to {action}.\nInvite link: https://discord.com/api/oauth2/authorize?client_id=931495315834548244&permissions=285379664&scope=bot%20applications.commands"
+	return guild, None
+
+# This is a common function in most commands to ensure that the current guild is valid and has an ongoing tournament.
+def findGuildTournament(guildId, action = "do this", tournamentNone = False):
+	guild, error = findGuild(guildId, action)
+	if not error is None: # Passes error from findGuild.
+		return None, error
+	if not tournamentNone and guildId != guild.id:
+		return None, f"You can't {action} because the ongoing tournament is in another server. Multiple tournaments are currently not supported, sorry."
+	
+	# Looks if specified tournament exists. tournamentNone decides if the tournament should exists; if it doesn't match the result, an error message is returned instead of None.
+	if (tournaments.get(guildId) is None) != tournamentNone:
+		if tournamentNone: # Decides if globalTournament should be None.
+			return None, f"You can't {action} because a tournament already exists, use `/tournament delete` to stop it."
+		else:
+			return None, f"You can't {action} because no tournament currently exists, use `/tournament create` to start one."
+	return guild, None # Returns the guild object, current global tournament variable (which should be by reference since objects are mutable), and a potential error message.
 
 # Looks for the channel/category name in existingList, creates one if it's missing, and returns it. Filter function can be used as an additional filter.
 async def findNameAsync(name, existingList, createFunctionAsync, filterFunction = None):
@@ -108,21 +131,10 @@ async def findOrCreateNamesAsync(names, existingList, createFunctionAsync, filte
 			targets[name] = await createFunctionAsync(name)
 	return targets.values() # Only returns the created objects.
 
+# Does the actual slash commands.
+
 slash = SlashCommand(client, sync_commands = True)
 guild_ids = [732240720487776356]
-
-#@slash.subcommand(
-#	base = "create",
-#	name = "channels",
-#	description = "Creates all necessary text and voice channels for the bot to function, if needed.",
-#	guild_ids = guild_ids)
-#async def createChannelsCommand(context):
-#	guild = findGuild(context.guild_id, "create a tournament")
-#	if not isinstance(guild, discord.Guild):
-#		await context.send(guild)
-#		return
-#	await createChannels(guild)
-#	await context.send(f"Created all necessary channels, use `/join` to start teams.")
 
 @slash.subcommand(
 	base = "tournament",
@@ -130,22 +142,17 @@ guild_ids = [732240720487776356]
 	guild_ids = guild_ids
 )
 async def createTournamentCommand(context):
-	guild = findGuild(context.guild_id, "create a tournament")
-	if not isinstance(guild, discord.Guild):
-		await context.send(guild)
+	guild, error = findGuildTournament(context.guild_id, "create a tournament", True)
+	if not error is None:
+		await context.send(error)
 		return
-	global tournament
-	if not tournament is None:
-		await context.send("A tournament is already in progress, use `/tournament delete` to stop it.")
-		return
-	
 	category = await findNameAsync("tournament", guild.categories, guild.create_category)
 	infoText, generalText = await findOrCreateNamesAsync(["info", "general"], category.text_channels, category.create_text_channel)
 	generalVoice = await findNameAsync("general", category.voice_channels, category.create_voice_channel)
 
-	tournament = Tournament(guild.id, infoText.id, generalVoice.id)
+	global tournaments
+	tournaments[context.guild_id] = Tournament(guild.id, infoText.id, generalVoice.id)
 	await context.send("Successfully created a tournament.")
-
 
 @slash.subcommand(
 	base = "tournament",
@@ -153,18 +160,12 @@ async def createTournamentCommand(context):
 	guild_ids = guild_ids
 )
 async def deleteTournamentCommand(context):
-	global tournament
-	if tournament is None:
-		await context.send("No tournament currently exists.")
+	error = findGuildTournament(context.guild_id, "delete a tournament")[1]
+	if not error is None:
+		await context.send(error)
 		return
-	guild = findGuild(context.guild_id, "delete a tournament")
-	if not isinstance(guild, discord.Guild):
-		await context.send(guild)
-		return
-	if tournament.guildId != guild.id:
-		await context.send("The ongoing tournament is currently in another server. Multiple tournaments are currently not supported, sorry.")
-		return
-	tournament = None
+	global tournaments
+	del tournaments[context.guild_id]
 	await context.send("Successfully deleted tournament.")
 
 @slash.subcommand(
@@ -173,30 +174,38 @@ async def deleteTournamentCommand(context):
 	guild_ids = guild_ids
 )
 async def tournamentDetailsCommand(context):
-	global tournament
-	if tournament is None:
-		await context.send("No tournament currently exists, use `/tournament create` to make one.")
+	error = findGuildTournament(context.guild_id, "see the ongoing tournament")[1]
+	if not error is None:
+		await context.send(error)
 		return
-	await context.send(str(tournament))
+	await context.send(str(tournaments[context.guild_id]))
+
+@slash.subcommand(
+	base = "team",
+	name = "create",
+	guild_ids = guild_ids
+)
+async def teamCreateCommand(context):
+	guild, error = findGuildTournament(context.guild_id, "create a team")
+	if not error is None:
+		await context.send(error)
+		return
+	await context.send("Will do, later.")
 
 @slash.slash(
 	name = "join",
 	guild_ids = guild_ids
 )
 async def joinCommand(context):
-	global tournament
-	if tournament is None:
-		await context.send("No tournament currently exists.")
+	guild, error = findGuild(context.guild_id, "join a tournament")
+	if not error is None:
+		await context.send(error)
 		return
-	guild = findGuild(context.guild_id, "join a tournament")
-	if not isinstance(guild, discord.Guild):
-		await context.send(guild)
-		return
-	if tournament.guildId != guild.id:
-		await context.send("The ongoing tournament is currently in another server. Multiple tournaments are currently not supported, sorry.")
-		return
-	tournament.lobby.append(Player(context.author.id))
+	global tournaments
+	tournaments[context.guild_id].lobby.append(Player(context.author.id))
 	await context.send(f"Successfully added {context.author.mention} to the lobby.")
+
+# Tries to run the client.
 
 try:
 	client.run(TOKEN)
